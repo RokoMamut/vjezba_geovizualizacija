0bb9d92533277714320a3c57b41abb94cc4f95c5
RokoMamut <roko.mamut123@gmail.com>
2025-05-19 17:35:08 +0200

    first upload

---
+++ data/autobusnikolodvor_10.js
@@ -0,0 +1,1 @@
+var json_autobusnikolodvor_10 = {"type":"FeatureCollection","name":"autobusnikolodvor_10","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[15.23868258192967,44.1076375409299]}}]}
